bb856953706d9c6fa277cda7849151a9d67e8a3c
divyagharat <divyaghrt@gmail.com>
2023-10-18 17:05:27 +0530

    First commit Employee.java file is uploded.

--- src/com/edubridge/app1/model/Employee.java
+++ src/com/edubridge/app1/model/Employee.java
@@ -1,5 +1,21 @@
 package com.edubridge.app1.model;
 
 public class Employee {
+private int  employyeId;
+private String employeeName;
+private double employeeSalary;
 
+
+
+public Employee(int employyeId, String employeeName, double employeeSalary) {
+	super();
+	this.employyeId = employyeId;
+	this.employeeName = employeeName;
+	this.employeeSalary = employeeSalary;
+}
+@Override
+public String toString() {
+	return "Employee [employyeId=" + employyeId + ", employeeName=" + employeeName + ", employeeSalary="
+			+ employeeSalary + "]";
+}
 }
